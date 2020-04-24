9be914e99aff827b03bafc93fd9cd312e71a250a
de-jure <de.jure.software@gmail.com>
2020-04-24 16:15:48 +0300

    Отрисока кнопок

--- Calendar/main.py
+++ Calendar/main.py
@@ -1,5 +1,6 @@
-from datetime import datetime
 import sys
+import calendar
+from datetime import datetime
 from PyQt5.QtWidgets import *
 from PyQt5 import QtGui
 from PyQt5.QtCore import QSize, Qt, QRect
@@ -16,6 +17,23 @@ class MainWindow(QMainWindow):
 		self.setCentralWidget(central_widget)           # Устанавливаем центральный виджет
 		grid_layout = QGridLayout()                     # Создаём сетку
 		central_widget.setLayout(grid_layout)
+		now = datetime.now()
+		days_in_month = calendar.monthrange(now.year, now.month)[1]
+		
+		# Создание интерфейса
+		buttons_days = []
+		for i in range(days_in_month):
+			buttons_days.append(i+1)
+		j = 0
+		while True:
+			for i in range(7):
+				try:
+					grid_layout.addWidget(QPushButton(str(buttons_days.pop(0)), self), j, i)
+				except:
+					break
+			if buttons_days == []:
+				break
+			j += 1
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
