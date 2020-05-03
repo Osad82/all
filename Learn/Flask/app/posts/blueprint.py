77748f9bc596e8a3079c27028ff81c301dc94d05
de-jure <de.jure.software@gmail.com>
2020-05-03 17:53:02 +0300

    Flask. 8 Урок.

--- Learn/Flask/app/posts/blueprint.py
+++ Learn/Flask/app/posts/blueprint.py
@@ -2,14 +2,45 @@ from flask import Blueprint
 from flask import render_template
 
 from models import Post, Tag
+from .forms import PostForm
+
+from flask import request
+from app import db
+
+from flask import redirect
+from flask import url_for
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
+@posts.route('/create', methods=['POST', 'GET'])
+def create_post():
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        try:
+            post = Post(title=title, body=body)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print('Error')
+
+        return redirect(url_for('posts.index'))
+
+    form = PostForm()
+    return render_template('posts/create_post.html', form=form)
+
 
 @posts.route('/')
 def index():
-    posts = Post.query.all()
+    q = request.args.get('q')
+
+    if q:
+        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q))
+    else:
+        posts = Post.query.order_by(Post.created.desc())
     return render_template('posts/index.html', posts=posts)
 
 
@@ -25,3 +56,4 @@ def tag_detail(slug):
     tag = Tag.query.filter(Tag.slug==slug).first()
     posts = tag.posts.all()
     return render_template('posts/tag_detail.html', tag=tag, posts=posts)
+
