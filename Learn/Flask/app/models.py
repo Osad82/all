324a1c64d8b8ae3749edc60adc955e5ad8318fe5
de-jure <de.jure.software@gmail.com>
2020-05-03 16:42:18 +0300

    Flask. 7 Урок

--- Learn/Flask/app/models.py
+++ Learn/Flask/app/models.py
@@ -2,9 +2,19 @@ from app import db
 from datetime import datetime
 import re
 
+
+
 def slugify(s):
     pattern = r'[^\w+]'
     return re.sub(pattern, '-', s)
+
+
+
+post_tags = db.Table('post_tags',
+                     db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+    )
+
 
 
 class Post(db.Model):
@@ -18,9 +28,23 @@ class Post(db.Model):
         super(Post, self).__init__(*args, **kwargs)
         self.generate_slug()
 
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
+
     def generate_slug(self):
         if self.title:
             self.slug = slugify(self.title)
 
     def __repr__(self):
         return '<Post id: {}, title: {}>'.format(self.id, self.title)
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100))
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return '<Tag id: {}, name: {}>'.format(self.id, self.name)
